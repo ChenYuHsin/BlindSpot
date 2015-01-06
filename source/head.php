ba3c37054463dbc7eea4a4262d209671761984e4
EddieWen-Taiwan <m20021991y@gmail.com>
2015-01-06 23:31:08 +0800

    head add some note

--- source/head.php
+++ source/head.php
@@ -15,12 +15,20 @@
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'>
 
+	<!-- PURE -->
 	<link type="text/css" rel="stylesheet" href="./lib/pure/pure-min.css" />
+
+	<!-- font-awesome -->
 	<link type="text/css" rel="stylesheet" href="./lib/font-awesome/css/font-awesome.css" />
+
+	<!-- my CSS -->
 	<link type="text/css" rel="stylesheet" href="./css/blind.css" />
 	<link type="text/css" rel="stylesheet" href="./css/responsive.css" />
 
+	<!-- jQuery -->
 	<script type="text/javascript" src="./lib/jquery.min.js" ></script>
+
+	<!-- my JS -->
 	<script type="text/javascript" src="./js/blind.js" ></script>
 
 	<script type="text/javascript" src="./lib/fallwall/fallwall.js"></script>
