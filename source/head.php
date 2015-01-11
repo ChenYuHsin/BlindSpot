8cfe1618540f433a2eb103a0d2e676b2e4f87601
EddieWen-Taiwan <m20021991y@gmail.com>
2015-01-11 18:22:31 +0800

    Can't resize in mobile

--- source/head.php
+++ source/head.php
@@ -8,8 +8,8 @@
 	<!-- meta settings -->
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> -->
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="Shortcut Icon" type="image/x-icon" href="./images/logo/blindspot-xicon.png" />
 
 	<!--   Google Font - Open sans   -->
